28f05c65096ce3678e25320322b2f996be0d4ab2
Tri Nguyen <treenguyen96@gmail.com>
2017-02-12 05:59:12 -0800

    added background

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ function love.load()
   Object = require "classic"
   require "game"
   engine = game();
+  background = love.graphics.newImage("Background.png")
 
 end
 
@@ -12,5 +13,10 @@ function love.update(dt)
 end
 
 function love.draw(dt)
+  for i = 0, love.graphics.getWidth() / background:getWidth() do
+        for j = 0, love.graphics.getHeight() / background:getHeight() do
+            love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
+        end
+    end
   game:draw(dt)
 end
