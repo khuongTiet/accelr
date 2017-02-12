6f56704264cf1ccb74e84fa96f6a659d587ca7b1
ktiet1 <ktiet1@uci.edu>
2017-02-11 16:14:01 -0800

    added x and y for tile and fixed draw function

--- tile.lua
+++ tile.lua
@@ -1,15 +1,15 @@
 tile = Object:extend()
 
 function tile:new()
-    self.x = x
-    self.y = y
+    self.x = 30
+    self.y = 100
 end
 
 function tile:update(dt)
 
 end
 
-function tile.draw(dt)
+function tile:draw(dt)
   love.graphics.setColor(122, 255, 255)
   love.graphics.rectangle("fill", self.x, self.y, 60, 30)
 end
