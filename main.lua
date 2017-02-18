56286097dc9b4111b07f0dbf4aa261f6e08f3394
khuongTiet <ktiet1@uci.edu>
2017-02-18 00:17:58 -0800

    new bg and tiles

--- main.lua
+++ main.lua
@@ -6,8 +6,8 @@ function love.load()
   world = love.physics.newWorld(0, 0, true)
   world:setCallbacks(beginContact, endContact)
   engine = game();
-  background = love.graphics.newImage("/assets/Background.png")
-  tileImage = love.graphics.newImage("/assets/Tile.png")
+  background = love.graphics.newImage("/assets/bg.png")
+  tileImage = love.graphics.newImage("/assets/tile2.png")
   sunnyImage = love.graphics.newImage("/assets/sunny.png")
   paddleImage = love.graphics.newImage("/assets/paddle1.png")
   gameStatus = ""
@@ -34,10 +34,6 @@ function love.update(dt)
 end
 
 function love.draw(dt)
-  for i = 0, love.graphics.getWidth() / background:getWidth() do
-        for j = 0, love.graphics.getHeight() / background:getHeight() do
-            love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
-        end
-    end
+  love.graphics.draw(background, 0, 0)
   game:draw(dt)
 end
